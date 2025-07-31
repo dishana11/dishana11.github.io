daec19d42b17039e03376ffa4b9261dd1cc283f8
Dishana <128571089+dishana11@users.noreply.github.com>
2025-08-01 03:20:06 +0530

    Update glow-control.js

--- assets/glow-control.js
+++ assets/glow-control.js
@@ -10,11 +10,11 @@ document.addEventListener('DOMContentLoaded', function () {
     // Reset animation by forcing reflow
     glow1.style.animation = 'none';
     glow1.offsetWidth; // Trigger reflow
-    glow1.style.animation = 'lightflow 1s linear';
+    glow1.style.animation = 'lightflow 4s linear';
 
     glow2.style.animation = 'none';
     glow2.offsetWidth;
-    glow2.style.animation = 'lightflow2 1s linear';
+    glow2.style.animation = 'lightflow2 7s linear';
 
     // Stop after it's done so it doesn't loop endlessly
     setTimeout(() => {
@@ -25,5 +25,5 @@ document.addEventListener('DOMContentLoaded', function () {
 
   animateGlow(); // Run initially once
 
-  setInterval(animateGlow, 12000); // Trigger every 12s
+  setInterval(animateGlow, 20000); // Trigger every 12s
 });
